2829fdaa2af5c30a7b45569512aeb1777944ebf9
smz-exe <shimizu-yuki13861@keio.jp>
2025-12-23 20:12:50 +0900

    add prettier and related plugins; configure prettier settings

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+const config = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+  tailwindStylesheet: "./src/styles/global.css",
+};
+
+export default config;
